6fe67d7de345fdd5aa5ebdccc84f256288a58889
김도국 <kdk9208@naver.com>
2026-02-12 14:37:10 +0900

    todolist - 실습과제

---
+++ Desktop/_react_todo/src/App.jsx
@@ -0,0 +1,151 @@
+import { useEffect, useRef, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todo, setTodo] = useState([
+    {
+      id: Number(new Date()),
+      content: "안녕하세요",
+    },
+  ]);
+
+  return (
+    <>
+      <Advice />
+      <Stopwatch />
+      <TodoInput setTodo={setTodo} />
+      <TodoList todo={todo} setTodo={setTodo} />
+    </>
+  );
+}
+
+const useFetch = (url) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+        setIsLoading(false);
+      });
+  }, [url]);
+  return [isLoading, data];
+};
+const Advice = () => {
+  const [isLoading, data] = useFetch(
+    "https://korean-advice-open-api.vercel.app/api/advice",
+  );
+
+  return (
+    <>
+      {!isLoading && (
+        <>
+          <div>{data.message}</div>
+          <div>-{data.author}-</div>
+        </>
+      )}
+    </>
+  );
+};
+
+const Clock = () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+  }, []);
+
+  return <div>{time.toLocaleTimeString()}</div>;
+};
+
+const formatitTime = (seconds) => {
+  const timeString = `${String(Math.floor(seconds / 3600)).padStart(2, "0")}:
+  ${String(Math.floor((seconds % 3600) / 60)).padStart(2, "0")}:
+  ${String(seconds % 60).padStart(2, "0")}`;
+  return timeString;
+};
+
+const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    if (isOn === true) {
+      const timerId = setInterval(() => {
+        setTime((prev) => prev + 1);
+      }, 1000);
+      timerRef.current = timerId;
+    } else {
+      clearInterval(timerRef.current);
+    }
+  }, [isOn]);
+
+  return (
+    <div>
+      {formatitTime(time)}
+      <button onClick={() => setIsOn((prev) => !prev)}>
+        {isOn ? "끄기" : "켜기"}
+      </button>
+      <button
+        onClick={() => {
+          setTime(0);
+          setIsOn(false);
+        }}
+      >
+        리셋
+      </button>
+    </div>
+  );
+};
+
+const TodoInput = ({ setTodo }) => {
+  const inputRef = useRef(null);
+  const addTodo = () => {
+    const newTodo = {
+      id: Number(new Date()),
+      content: inputRef.current.value,
+    };
+    setTodo((prev) => [...prev, newTodo]);
+  };
+
+  return (
+    <>
+      <input ref={inputRef} />
+      <button onClick={addTodo}>추가</button>
+    </>
+  );
+};
+
+const TodoList = ({ todo, setTodo }) => {
+  return (
+    <ul>
+      {todo.map((el) => (
+        <Todo key={el.id} todo={el} setTodo={setTodo} />
+      ))}
+    </ul>
+  );
+};
+
+const Todo = ({ todo, setTodo }) => {
+  return (
+    <>
+      <li>
+        {todo.content}
+        <button
+          onClick={() => {
+            setTodo((prev) => prev.filter((el) => el.id !== todo.id));
+          }}
+        >
+          삭제
+        </button>
+      </li>
+    </>
+  );
+};
+
+export default App;
